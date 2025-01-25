707d146497f7a7ca0d7247fc1c010f6cf22e07f8
Krishna Kaushik <96732448+krishnakaushik195@users.noreply.github.com>
2025-01-25 15:21:48 +0530

    Update openwebUi.py

--- openwebUi.py
+++ openwebUi.py
@@ -7,7 +7,7 @@ import time
 
 # Define database URIs
 db_uris = {
-    "sys": 'mysql+mysqlconnector://root:Krishna%40195@host.docker.internal:3306/sys',
+    "sys": 'mysql+mysqlconnector://root:Krishna%40195@host.docker.internal:3306/krishna',
     "chinook": 'mysql+mysqlconnector://root:Krishna%40195@host.docker.internal:3306/chinook',
     "sakila": 'mysql+mysqlconnector://root:Krishna%40195@host.docker.internal:3306/sakila'
 }
